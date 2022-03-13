24514c290e1044b23fedfc34d6017f791bbdd901
Ruth Bonilla <ruthebonilla01@gmail.com>
2022-03-13 03:07:08 -0400

    react form-validation assign(will work on later)

---
+++ React/form-validation/src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import useReducer from './components/useReducer'
+
+function App() {
+  return (
+    <div className="App">
+      <useReducer />
+    </div>
+  );
+}
+
+export default App;
---
+++ React/form-validation/src/components/useReducer.js
@@ -0,0 +1,25 @@
+import React, { useReducer } from 'react';
+
+const initialState = {
+    firstName: {
+        value: "",
+        error: null
+    },
+    lastName: {
+        value: "",
+        error: null
+    },
+    email: {
+        value: "",
+        error: null
+    }
+};
+
+
+
+
+
+
+
+
+export default useReducer;
