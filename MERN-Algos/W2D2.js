8332af87baabe81328ab9b82cf18cab63b9ea7f1
Ruth Bonilla <ruthebonilla01@gmail.com>
2022-03-15 18:43:52 -0400

    algo not finished

---
+++ MERN-Algos/W2D2.js
@@ -0,0 +1,29 @@
+//Create a standalone function that accepts a string
+//and an integer, and rotates the characters in the
+//string to the right by that amount.
+//this one must be done without built in methods
+//("Did I shine my shoes today?", 9) ->
+//"es today?Did I shine my sho"
+const rotateString = (str, num) => {
+    let newString = "";
+    for(let i = 0; i < str.length; i++){
+        console.log(`add this: ${str[(i + str.length - num) % str.length]}`);
+        newString = newString + str[(i + str.length - num) % str.length];
+    }
+    return newString;
+}
+
+// let testString = "012345";
+// console.log(rotateString(testString, 3));
+let shoes = "Did I shine my shoes today?";
+console.log(rotateString(shoes, 9));
+
+//write a function that will return true if str2 is a
+//rotation of str1. otherwise return false
+//("Did I shine my shoes today?", "es today?Did I shine my shoe")
+// -> returns true
+const isRotation = (str1, str2) => {
+}
+
+console.log(isRotation("Did I shine my shoes today?", "es today?Did I shine my sho"));
+console.log(isRotation("Did I shine my shoes today? ", "es today?Did I shine my sho"));
